fea77346e0fc702fab2182e31e97563df782a745
Mike Guida <mike@mguida.com>
2024-07-21 18:05:33 -0600

    organize server

--- server/main.go
+++ server/main.go
@@ -3,30 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 )
+
+func main() {
+	r := mux.NewRouter()
+
+	r.Use(loggingMiddleware)
+
+	r.HandleFunc("/health", healthHandler).Methods("GET")
+	r.HandleFunc("/{userId}/activities", activitiesHandler).Methods("GET")
+	r.HandleFunc("/{userId}/activities/{activityId}", activityHandler).Methods("GET")
+
+	log.Println("Server started on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
-}
-
-func main() {
-	r := mux.NewRouter()
-
-	if os.Getenv("ENV") == "development" {
-		r.Use(loggingMiddleware)
-	}
-
-	r.HandleFunc("/{userId}/activities", activitiesHandler).Methods("GET")
-	r.HandleFunc("/{userId}/activities/{activityId}", activityHandler).Methods("GET")
-
-	log.Println("Server started on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func activitiesHandler(w http.ResponseWriter, r *http.Request) {
@@ -40,4 +38,9 @@ func activityHandler(w http.ResponseWriter, r *http.Request) {
 	userID := vars["userId"]
 	activityID := vars["activityId"]
 	w.Write([]byte("Activity ID: " + activityID + " for user ID: " + userID))
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
